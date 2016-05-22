c6d247308005d7af17edcf70623654a7870826db
LaureenSIOUSARRAN <laureensiousarran@ymail.com>
2016-05-22 18:02:46 +0200

    Add files via upload
    Nouvelle version de l'application avec les fonctions précédentes, en ayant implémenté les fonctions suivantes : le stockage des données en JSON, un bouton "Mise à jour" permettant de réinitialiser la liste en effaçant l'ensemble des pointages, la visualisation de l'ensemble des pointages effectuer, la mise en place du pointage (en évitant le cas où il y a trop de pointage). Il est possible aussi de trouver en commentaire ce qui pourra servir pour l''implémentation des fonctions aidant à la liaison avec l'application web.

--- www/js/app.js
+++ www/js/app.js
@@ -3,61 +3,182 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('MyApp', ['ionic'])
+angular.module('MyApp', ['ionic', 'ngStorage'])
 
-.controller('MyCtrl', function($scope,$ionicLoading,$state) {
+.factory ('StorageService', function ($localStorage) {
+
+  $localStorage = $localStorage.$default({
+    things: []
+  });
+  
+
+  var _getAll = function () {
+    return $localStorage.things;
+  };
+
+  var _add = function (thing) {
+    $localStorage.things.push(thing);
+  }
+
+  var _remove = function (thing) {
+    $localStorage.things.splice($localStorage.things.indexOf(thing), 1);
+  }
+
+  return {
+    getAll: _getAll,
+    add: _add,
+    remove: _remove
+  };
+})
+
+.controller('MyCtrl', function($scope,$http,$ionicLoading,$state,StorageService){
+
+  $scope.init = function(){
+	window.localStorage.removeItem('Hello');
+	 /*
+        $http.get("adresse fichier json")
+            .success(function(data) {
+                items=data;
+            })
+            .error(function(data) {
+                alert("ERROR");
+            })
+	*/
+  // à remplacer par requête http ci-dessus
+	var items = [{
+						"id":"0001",
+						"nom":"Matin",
+						"prenom":"Martin",
+						"age": 12,
+						"classe":"CE2",
+						"E1":"",
+						"S1":"",
+						"E2":"",
+						"S2":""
+					},
+					{
+						"id":"0002",
+						"nom":"Colin",
+						"prenom":"Carla",
+						"age": 13,
+						"classe":"CP",
+						"E1":"",
+						"S1":"",
+						"E2":"",
+						"S2":""
+					},
+					{
+						"id":"0006",
+						"nom":"Dupuis",
+						"prenom":"Louis",
+						"age": 6,
+						"classe":"CM2",
+						"E1":"",
+						"S1":"",
+						"E2":"",
+						"S2":""
+					},
+					
+					{
+						"id":"0003",
+						"nom":"Dupont",
+						"prenom":"Noemie",
+						"age": 8,
+						"classe":"CP",
+						"E1":"",
+						"S1":"",
+						"E2":"",
+						"S2":""
+					},
+					{
+						"id":"0004",
+						"nom":"Menier",
+						"prenom":"Elodie",
+						"age": 8,
+						"classe":"CP",
+						"E1":"",
+						"S1":"",
+						"E2":"",
+						"S2":""
+					},
+					{
+						"id":"0005",
+						"nom":"Marin",
+						"prenom":"David",
+						"age": 11,
+						"classe":"CP",
+						"E1":"",
+						"S1":"",
+						"E2":"",
+						"S2":""
+					}
+		];
+		alert('ok');
+	window.localStorage.setItem('Hello',JSON.stringify(items));  
+	$scope.enfants = JSON.parse(window.localStorage.getItem('Hello'));
+	alert('perfect');
+  }
   
   
   $scope.arriver = function(item) {
 	var date = new Date();
 	alert('Arrivée de '+ item.nom + ' à '+ date.getHours()+"h "+date.getMinutes()+"min "+date.getSeconds()+"s");
+	if (item.E1 == ""){
+		item.E1=date;
+	}else if(item.E2==""){
+		item.E2=date;
+	}else{
+		alert('Trop de pointage');
+	}
+	window.localStorage.setItem('Hello',JSON.stringify($scope.enfants)); 
+	$scope.enfants = JSON.parse(window.localStorage.getItem('Hello'));
   };
   $scope.sortir = function(item) {
 	var date = new Date();
     alert('Sortie de '+ item.nom + ' à '+ date.getHours()+"h "+date.getMinutes()+"min "+date.getSeconds()+"s");
+	if (item.S1 == ""){
+		item.S1=date;
+	}else if(item.S2==""){
+		item.S2=date;
+	}else{
+		alert('Trop de pointage');
+	}
   };
   
   $scope.plus = function(item) {
 	var nom=item.nom
+	var prenom=item.prenom
 	var age=item.age
 	var classe=item.classe
-	$state.go('fiche',{nom : nom,age : age, classe : classe});
+	var E1=item.E1
+	var E2=item.E2
+	var S1=item.S1
+	var S2=item.S2
+	$state.go('fiche',{nom : nom, prenom : prenom, age : age, classe : classe, E1 : E1, E2 : E2, S1 : S1, S2 : S2 });
   };
   
-  $scope.enfants = [{
-						"nom":"Martin",
-						"age": 12,
-						"classe":"CE2"
-					},
-					{
-						"nom":"Carla",
-						"age": 13,
-						"classe":"CP"
-					},
-					{
-						"nom":"Louis",
-						"age": 6,
-						"classe":"CM2"
-					},
-					
-					{
-						"nom":"Noemie",
-						"age": 8,
-						"classe":"CP"
-					},
-					{
-						"nom":"David",
-						"age": 11,
-						"classe":"CP"
-					}
-		];
+  $scope.enfants = JSON.parse(window.localStorage.getItem('Hello'));
   
+  $scope.envoyer=function(){
+	 /* $http.post("adresse fichier json",data)
+            .success(function() {
+                alert("ERROR");
+            })
+            .error(function() {
+                alert("ERROR");
+            });*/
+  }
 })
 
 .controller('FicheCtrl', function($scope,$ionicLoading,$state,$stateParams) {
 	 $scope.nom = $stateParams.nom;
+	 $scope.prenom=$stateParams.prenom;
 	 $scope.classe = $stateParams.classe;
 	 $scope.age = $stateParams.age;
+	 $scope.E1=$stateParams.E1;
+	 $scope.S1=$stateParams.S1;
+	 $scope.E2=$stateParams.E2;
+	 $scope.S2=$stateParams.S2;
 })
 
 .config(function($stateProvider,$urlRouterProvider){
@@ -68,7 +189,7 @@ angular.module('MyApp', ['ionic'])
 			templateUrl:'pages/home.html',
 			controller : 'MyCtrl'})
 			.state('fiche',{
-			url:'/fiche/:nom/:classe/:age',
+			url:'/fiche/:nom/:prenom/:classe/:age/:E1/:E2/:S1/:S2',
 			templateUrl:'pages/fiche.html',
 			controller : 'FicheCtrl'})
 })
