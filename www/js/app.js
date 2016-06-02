4bad876cad416293705f96d05461135e7f3ca81f
LaureenSIOUSARRAN <laureensiousarran@ymail.com>
2016-06-02 17:19:46 +0200

    Add files via upload
    Correction titre + correction concernant les heures pour une homogénéité des heures avec l'application web.

--- www/js/app.js
+++ www/js/app.js
@@ -93,7 +93,7 @@ angular.module('MyApp', ['ionic', 'ngStorage'])
 						"S2":""
 					}
 		];
-	window.localStorage.setItem('Hello',JSON.stringify(items));  
+	window.localStorage.setItem('Hello',JSON.stringify(items)); //stockage du tableau items sous format JSON sous la variable 'Hello' 
 	$scope.enfants = JSON.parse(window.localStorage.getItem('Hello'));
   }
   
@@ -102,9 +102,11 @@ angular.module('MyApp', ['ionic', 'ngStorage'])
   $scope.arriver = function(item) {
 	var date = new Date();
 	if (item.E1 == ""){
-		item.E1=date.getHours()+":"+date.getMinutes();
+		if(date.getMinutes()<10)item.E1=date.getHours()+":0"+date.getMinutes()+":00";
+		else item.E1=date.getHours()+":"+date.getMinutes()+":00";
 	}else if(item.E2==""){
-		item.E2=date.getHours()+":"+date.getMinutes();
+		if(date.getMinutes()<10)item.E2=date.getHours()+":0"+date.getMinutes()+":00";
+		else item.E2=date.getHours()+":"+date.getMinutes()+":00";
 	}else{
 		alert('Trop de pointage'); //POPUP //en cas de pointage de trop !
 	}
@@ -117,9 +119,11 @@ angular.module('MyApp', ['ionic', 'ngStorage'])
   $scope.sortir = function(item) {
 	var date = new Date();
 	if (item.S1 == ""){
-		item.S1=date.getHours()+":"+date.getMinutes();
+		if(date.getMinutes()<10)item.S1=date.getHours()+":0"+date.getMinutes()+":00";
+		else item.S1=date.getHours()+":"+date.getMinutes()+":00";
 	}else if(item.S2==""){
-		item.S2=date.getHours()+":"+date.getMinutes();
+		if(date.getMinutes()<10)item.S2=date.getHours()+":0"+date.getMinutes()+":00";
+		else item.S2=date.getHours()+":"+date.getMinutes()+":00";
 	}else{
 		alert('Trop de pointage'); //POPUP //en cas de pointage de trop !
 	}
@@ -184,19 +188,19 @@ angular.module('MyApp', ['ionic', 'ngStorage'])
 		var horaire = document.getElementById("horaire").value;
 		var pointage = document.getElementById('pointage').selectedIndex;
 		for (var name in tableau){
-		 if (tableau[name].nom == $scope.nom){ 
+		 if ((tableau[name].nom == $scope.nom) && (tableau[name].prenom == $scope.prenom)){ 
 			switch(pointage) {
 				case 0:
-					tableau[name].E1=horaire;
+					tableau[name].E1=horaire+":00";
 					break;
 				case 1:
-					tableau[name].S1=horaire;
+					tableau[name].S1=horaire+":00";
 					break;
 				case 2:
-					tableau[name].E2=horaire;
+					tableau[name].E2=horaire+":00";
 					break;
 				default:
-					tableau[name].S2=horaire;
+					tableau[name].S2=horaire+":00";
 					break;
 			}
 		}
