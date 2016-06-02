49eea9438c792d6840e41fb39f6e9652e16a03d2
NReveneau <reveneau.nicolas@gmail.com>
2016-06-02 10:54:05 +0200

    Add files via upload

--- eleve/admin.py
+++ eleve/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from eleve.models import *
 
 class Enfantadmin(admin.ModelAdmin):
-    list_display = ('date','lastname', 'firstname', 'classe')
+	list_display = ('date','lastname', 'firstname', 'classe')
+	search_fields = ('lastname', 'firstname',)
 
 admin.site.register(Enfant, Enfantadmin)
