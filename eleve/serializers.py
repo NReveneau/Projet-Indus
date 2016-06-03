f6b4539174a9490647eb2ed063fc134615fe2012
BastienDeneux <bastiendeneux1@gmail.com>
2016-06-03 11:20:48 +0200

    Update serializers.py

--- eleve/serializers.py
+++ eleve/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
+#serializer permettant la traduction des fields depuis le modèle Enfant en Json et inversement
 class EnfantSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Enfant
 		fields = ('Nindiv','nom','prenom','date','classe','E1','E2','S1','S2','age')
 
-
+#serializer permettant la traduction des fields depuis le modèle EnfantComplete en Json et inversement
 class EnfantCompleteSerializer(serializers.ModelSerializer):
 
 	class Meta:
