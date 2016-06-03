137634ac14b325c3338c42c85c397111ec41d3d8
NReveneau <reveneau.nicolas@gmail.com>
2016-06-03 09:06:26 +0200

    Add files via upload

--- eleve/urls.py
+++ eleve/urls.py
@@ -5,8 +5,13 @@ from eleve import views
 
 
 urlpatterns = [
-	url(r'^enfants/$', views.enfant_list, name = 'ajouter_enfant'),
+	url(r'^enfant/$', views.enfant_list, name = 'afficher_enfant'),
+	url(r'^ajouterenfant/$', views.ajouterEnfant, name = 'ajouter_enfant'),
+	url(r'^enfantcomplete/$', views.enfantcomplete_list, name = 'afficher_enfantcomplete'),
+	url(r'^ajouterenfantcomplete/$', views.ajouterEnfantComplete, name = 'ajouter_enfantcomplete'),
     url(r'^json/$',views.liste),
-    url(r'^json/(?P<Nindiv>[a-zA-Z0-9_;-]+)$',views.details)
+    url(r'^json/(?P<Nindiv>[a-zA-Z0-9_;-]+)$',views.details),
+    url(r'^jsoncomplete/$',views.listeComplete),
+    url(r'^jsoncomplete/(?P<Nindiv>[a-zA-Z0-9_;-]+)$',views.detailsComplete)
 ]
 
