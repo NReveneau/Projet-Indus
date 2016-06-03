358986ffebbf792e518c58658ab84fff857c3e24
BastienDeneux <bastiendeneux1@gmail.com>
2016-06-03 12:02:52 +0200

    Update urls.py

--- eleve/urls.py
+++ eleve/urls.py
@@ -5,13 +5,21 @@ from eleve import views
 
 
 urlpatterns = [
+	#création d'une adresse pour l'affichage de la liste des enfants avant pointage
 	url(r'^enfant/$', views.enfant_list, name = 'afficher_enfant'),
+	#création d'une adresse pour l'affichage du formulaire de création des enfants avant pointage
 	url(r'^ajouterenfant/$', views.ajouterEnfant, name = 'ajouter_enfant'),
+	#création d'une adresse pour l'affichage de la liste des enfants après pointage
 	url(r'^enfantcomplete/$', views.enfantcomplete_list, name = 'afficher_enfantcomplete'),
+	#création d'une adresse pour l'affichage ddu formulaire de création des enfants après pointage
 	url(r'^ajouterenfantcomplete/$', views.ajouterEnfantComplete, name = 'ajouter_enfantcomplete'),
-    url(r'^json/$',views.liste),
-    url(r'^json/(?P<Nindiv>[a-zA-Z0-9_;-]+)$',views.details),
-    url(r'^jsoncomplete/$',views.listeComplete),
-    url(r'^jsoncomplete/(?P<Nindiv>[a-zA-Z0-9_;-]+)$',views.detailsComplete)
+	#création d'une adresse pour l'affichage de la page des méthodes GET et POST pour le modèle Enfant
+ 	url(r'^json/$',views.liste),
+ 	#création d'une adresse pour l'affichage de la page des méthodes GET, PUT et DELETE pour le modèle Enfant
+    	url(r'^json/(?P<Nindiv>[a-zA-Z0-9_;-]+)$',views.details),
+    	#création d'une adresse pour l'affichage de la page des méthodes GET et POST pour le modèle EnfantComplete
+    	url(r'^jsoncomplete/$',views.listeComplete),
+    	#création d'une adresse pour l'affichage de la page des méthodes GET, PUT et DELETE pour le modèle EnfantComplete
+    	url(r'^jsoncomplete/(?P<Nindiv>[a-zA-Z0-9_;-]+)$',views.detailsComplete)
 ]
 
