09dc5061f96ccc9eb55368981473c97bb39d9ac5
BastienDeneux <bastiendeneux1@gmail.com>
2016-06-03 11:17:43 +0200

    Update forms.py

--- eleve/forms.py
+++ eleve/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from models import *
 
+#formulaire permettant de récupérer les informations notées dans fields appartenant au modèle Enfant
 class EnfantForm(forms.ModelForm):
 
 	class Meta:
 		model = Enfant
 		fields = ('nom','prenom','classe','date','E1','E2','S1','S2','Nindiv','age')
 
-
+#formulaire permettant de récupérer les informations notées dans fields appartenant au modèle EnfantComplete
 class EnfantCompleteForm(forms.ModelForm):
 
 	class Meta:
